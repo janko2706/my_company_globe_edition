85326c947c3e5341a7b5c169fa1b82f6e307a5b6
Jan Bogovic <bogovicjan@gmail.com>
2023-08-21 14:40:16 +0200

    fixed mobile issues and improved contact

--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -1,34 +1,36 @@
-import React from 'react';
-import './contacts.css';
-import contactPic from '../../Assets/Images/callToAction.png';
+import React from "react";
+import "./contacts.css";
+import contactPic from "../../Assets/Images/callToAction.png";
 
 const Contacts = () => {
   return (
-    <section className='contact' id='contact'>
-      <h1 class='section-header'>Contact</h1>
-      <div className='contactCont'>
-        <img src={contactPic} alt='call to action, no backgound' />
+    <section className="contact" id="contact">
+      <h1 class="section-header">Contact</h1>
+      <div className="contactCont">
+        <img src={contactPic} alt="call to action, no backgound" />
 
-        <div class='contact-wrapper'>
-          <div class='direct-contact-container'>
-            <ul class='contact-list'>
-              <li class='list-item'>
-                <span class='contact-text place'>Just-Pine Obrt</span>
+        <div class="contact-wrapper">
+          <div class="direct-contact-container">
+            <ul class="contact-list">
+              <li class="list-item">
+                <span class="contact-text place">
+                  JUST PINE, Obrt za racunalno programiranje
+                </span>
               </li>
 
-              <li class='list-item'>
-                <span class='contact-text phone'>
-                  <a href='tel:1-212-555-5555' title='Give me a call'>
+              <li class="list-item">
+                <span class="contact-text phone">
+                  <a href="tel:1-212-555-5555" title="Give me a call">
                     (+385) 992166806
                   </a>
                 </span>
               </li>
 
-              <li class='list-item'>
-                <span class='contact-text gmail'>
+              <li class="list-item">
+                <span class="contact-text gmail">
                   <a
-                    href='mailto:bogovicjan@gmail.com'
-                    title='Send me an email'
+                    href="mailto:bogovicjan@gmail.com"
+                    title="Send me an email"
                   >
                     bogovicjan@gmail.com
                   </a>
@@ -38,7 +40,7 @@ const Contacts = () => {
 
             <hr />
 
-            <div class='copyright'>&copy; ALL OF THE RIGHTS RESERVED</div>
+            <div class="copyright">&copy; ALL OF THE RIGHTS RESERVED</div>
           </div>
         </div>
       </div>
